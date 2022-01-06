8fc4d997b2d6f8a70ee6b2fe239342569070e130
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2022-01-06 11:55:13 -0800

    store message counter

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,17 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    constructor() {
-        console.log("Smart contract console log");
-    }
+	uint256 totalWaves;
+	constructor() {
+		console.log("Smart contract console log");
+	}
+	function wave() public {
+		totalWaves += 1;
+		console.log("%s has waved!", msg.sender);
+	}
+
+	function getTotalWaves() public view returns (uint256) {
+		console.log("We have %d total waves!", totalWaves);
+		return totalWaves;
+	}
 }
--- scripts/run.js
+++ scripts/run.js
@@ -3,16 +3,16 @@ const main = async () => {
 	const waveContract = await waveContractFactory.deploy();
 	await waveContract.deployed();
 	console.log("Contract deployed to:", waveContract.address);
-  };
-  
-  const runMain = async () => {
+};
+
+const runMain = async () => {
 	try {
-	  await main();
-	  process.exit(0);
+	await main();
+	process.exit(0);
 	} catch (error) {
-	  console.log(error);
-	  process.exit(1);
+	console.log(error);
+	process.exit(1);
 	}
-  };
-  
-  runMain();
+};
+
+runMain();
